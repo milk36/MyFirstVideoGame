54e8dd2cc9fa39e67b135778e50d302c4c7241ca
milk_36 <milk_36@163.com>
2024-06-01 09:00:29 +0800

    1.初始工程

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    Debug.Log("Start Game!");
+    // rb.useGravity = false;
+  }
+
+  // 固定帧,用于执行物理碰撞等逻辑 
+  void FixedUpdate()
+  {
+    rb.AddForce(0, 0, 2000 * Time.deltaTime);//deltaTime 平均每帧的速度
+  }
 }
